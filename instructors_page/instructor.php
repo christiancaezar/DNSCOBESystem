8ce62893ee36c90755e0cbec0a705033794353b6
christiancaezar <christiancaezar@gmail.com>
2024-03-14 10:27:44 +0800

    added search bar

--- instructors_page/instructor.php
+++ instructors_page/instructor.php
@@ -5,37 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Instructor</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../global.css">
-    <style>
-        
-    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="../navbar/navbar.css">
+    <link rel="stylesheet" href="instructor.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <div class="container d-flex align-items-center">
-                <a href=""><img src="../assets/dnsclogo.png" class="image"></a>
-                <a class="navbar-brand" href="#">DNSC OBE System</a>
-            </div> 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link act disabled" aria-current="page" href="#">Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Instructors</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Course Subjects</a>
-                    </li>
-                </ul>
+    <?php include '../navbar/navbar.php'; ?>
+    <div class="container-fluid buttons">
+        <div class="row">
+            <div class="col-9 me-1  ">
+                <div class="container d-flex search">
+                    <i class="bi bi-search fs-3"></i>
+                    <input type="email" class="form-control email" id="exampleFormControlInput1" placeholder="Search">
+                </div>
+            </div>
+            <div class="col-3">
+
             </div>
         </div>
-    </nav>
-
+    </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
---
+++ navbar/navbar.php
@@ -0,0 +1,25 @@
+
+<nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+            <div class="container d-flex align-items-center">
+                <a href=""><img src="../assets/dnsclogo.png" class="image"></a>
+                <a class="navbar-brand" href="#">DNSC OBE System</a>
+            </div> 
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link act disabled" href="#">Instructors</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Course Subjects</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
