44da7ed31ec3e0cdfa08b80fbd6e72663a470337
Sferaaaa <gorez.rogie14@gmail.com>
2024-04-01 21:34:21 +0800

    index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></head>
-
+    <link rel="stylesheet" href="navbar/navbar.css">
     <style>
         *{
         box-sizing: border-box;
@@ -16,81 +16,63 @@
         }
 
         .upperBody{
-        /* height: ; */
         display: flex;
         justify-content: center;
         align-items: center;
         overflow: hidden;
-        background-color: black;
         height: 300px;
         }
 
         .upperBody img{
-            width: 100vw;
-  height: 100%;
-  object-fit: contain;
-        /* flex-shrink: 0; */
-        /* min-width: 100vw; */
-        /* min-height: 100% */
-
+          width: 100vw;
+          height: 100%;
+          object-fit: contain;
+          
         }
 
-        .secondBorder{
-        height: 30px; 
-        background-color:  green;
-        margin-bottom: 100px;
-
+        
+        .bottom-container{
+          width: 100%;
+          min-height: max-content;
+          padding-top: 50px;
+       
         }
 
+        
+
+        .bgPic2{
+          position: absolute;
+          z-index: -5;
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+        }
+        hr{
+          margin: 0px;
+          height: 30px;
+          background-color: #1F4727;
+          opacity: 100%;
+          border: none;
+        }
+
+      
+       
 
     </style>
     <body>
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">Navbar</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><hr class="dropdown-divider"></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                  </li>
-                </ul>
-                <form class="d-flex" role="search">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                  <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-              </div>
-            </div>
-          </nav>
+    <?php include 'navbar/navbar.php'; ?>
+        
        
 <div class="upperBody">
         <div class="bgPic">
-            <img src="img/KNT01281-2.jpg" alt="BackGroundPic">
-
+            <img src="img/KNT01281-5.png" alt="BackGroundPic">
         </div>
-</div>
-<pre style="height: 50px;"></pre>
 
-<div class="columns">
+</div>
+
+<hr>
+
+<div class="bottom-container">
     <div class="container text-center">
         <div class="row align-items-end">
           <div class="col">
@@ -145,17 +127,23 @@
       <div class="container text-center">
         <div class="row align-items-end">
         <div class="col">
-            Program Outcome 13
+           <!-- Program Outcome 13 -->
+            <div class="progress" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+            <div class="progress-bar bg-success" style="width: 25%"></div>
+            </div>
         </div>
-
         </div>
       </div>
+
+      <div class="circular-progress-div">
+        
+      </div>
+
 </div>
-
-
-
+<div class="fixed-bottom">
+<hr>
+</div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
-
 </html>
