848d02e060cc85980d1aa6017d1fd636afea39f1
Ian Cezar Ortega <christiancaezar@gmail.com>
2024-04-01 21:36:17 +0800

    changed navbar logo referencing

--- navbar/navbar.php
+++ navbar/navbar.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <div class="container d-flex align-items-center">
-                <a href=""><img src="../assets/dnsclogo.png" class="image"></a>
+                <a href=""><img src="<?php echo $location;?>/dnsclogo.png" class="image"></a>
                 <a class="navbar-brand" href="#">DNSC OBE System</a>
             </div> 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
