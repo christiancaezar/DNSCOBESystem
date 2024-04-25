23f328a38676df91d19feb63c45ab9c1835850c7
Ian Cezar Ortega <christiancaezar@gmail.com>
2024-04-25 22:05:16 +0800

    fixed navbar

--- navbar/navbar.php
+++ navbar/navbar.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <div class="container d-flex align-items-center">
-                <a href=""><img src="assets/dnsclogo.png" class="image"></a>
+                <a href=""><img src="/DNSCOBESystem/assets/dnsclogo.png" class="image"></a>
                 <a class="navbar-brand" href="#">DNSC OBE System</a>
             </div> 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,13 +11,13 @@
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="../index.php" id="dashboard">Dashboard</a>
+                        <a class="nav-link" aria-current="page" href="/DNSCOBESystem/index.php" id="dashboard">Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/instructors_page/instructor.php" id="instructor">Instructors</a>
+                        <a class="nav-link" href="/DNSCOBESystem/instructors_page/instructor.php" id="instructor">Instructors</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/course_subject_page/course_subject.php" id="course">Course Subjects</a>
+                        <a class="nav-link" href="/DNSCOBESystem/course_subject_page/course_subject.php" id="course">Course Subjects</a>
                     </li>
                 </ul>
             </div>
